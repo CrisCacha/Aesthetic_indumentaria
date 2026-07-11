8d72d863e0b89bc62cace34c805810fea5910fc2
CrisCacha <124098365+CrisCacha@users.noreply.github.com>
2026-07-11 16:23:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Página Aesthetic cargada correctamente");
+
+
+document.querySelector("button").addEventListener("click",()=>{
+
+alert("Próximamente podrás ver nuestra colección");
+
+});
